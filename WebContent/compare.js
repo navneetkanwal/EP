e1111b7c1c1878b8139b59c5e49d226f682db532
Navneet Kanwal <Navneet Kanwal@NAVNEET-PC>
2017-07-25 00:55:59 +1000

    filter & design

--- WebContent/compare.js
+++ WebContent/compare.js
@@ -5,86 +5,88 @@ var url_safe_usage;
 
 window.onload = function() {
 
-
 };
 
 var app = angular.module('myApp', []);
 
-
-
 app.filter('unique', function() {
-	   // we will return a function which will take in a collection
-	   // and a keyname
-	   return function(collection, keyname) {
-	      // we define our output and keys array;
-	      var output = [], 
-	          keys = [];
-	      
-	      // we utilize angular's foreach function
-	      // this takes in our original collection and an iterator function
-	      angular.forEach(collection, function(item) {
-	          // we check to see whether our object exists
-	          var key = item[keyname];
-	          // if it's not already part of our keys array
-	          if(keys.indexOf(key) === -1) {
-	              // add it to our keys array
-	              keys.push(key); 
-	              // push this item to our final output array
-	              output.push(item);
-	          }
-	      });
-	      // return our array which should be devoid of
-	      // any duplicates
-	      return output;
-	   };
-	});
+	// we will return a function which will take in a collection
+	// and a keyname
+	return function(collection, keyname) {
+		// we define our output and keys array;
+		var output = [], keys = [];
 
-
-app.controller('myCtrl',
-		function($scope) {
-	var companyList = document.getElementById("companyList");
-			ratelist = rateListMethod();
-			if (decodeURIComponent(window.location.search.substring(1)).split(
-					"&").length > 1) {
-				url_safe_distributor = decodeURIComponent(
-						window.location.search.substring(1)).split("&")[0]
-						.split("=")[1];
-				url_safe_tarifftype = decodeURIComponent(
-						window.location.search.substring(1)).split("&")[1]
-						.split("=")[1];
-				url_safe_usage = decodeURIComponent(
-						window.location.search.substring(1)).split("&")[2]
-						.split("=")[1];
-				url_safe_days = decodeURIComponent(
-						window.location.search.substring(1)).split("&")[3]
-						.split("=")[1];
+		// we utilize angular's foreach function
+		// this takes in our original collection and an iterator function
+		angular.forEach(collection, function(item) {
+			// we check to see whether our object exists
+			var key = item[keyname];
+			// if it's not already part of our keys array
+			if (keys.indexOf(key) === -1) {
+				// add it to our keys array
+				keys.push(key);
+				// push this item to our final output array
+				output.push(item);
 			}
-			var plans = 0;
-			var spend = 0;
-			var res = jQuery.grep(ratelist.Rates, function(v) {
-				if (v.Distributor == url_safe_distributor
-						&& v.TariffType == url_safe_tarifftype) {
-					plans++;
-					spend = ((v.Peak1*url_safe_usage) + (v.Supply * url_safe_days));
-					v.spend = parseFloat(spend);
-					return v;
-				}
-			});
-			res.sort(function(a, b){
-			    return a.spend - b.spend;
-			});
-			res["Rates"] = res;
-			$scope.jsonbody = res;
-			$scope.plans = plans;
-			if (plans > 0) {
-				$scope.rate = "Rates";
-				$scope.exitfee = "Exit Fee";
-				$scope.discount = "Discount";
-			
-			}
-		
-			   $scope.clearFilter = function() {
-				     
-				      $scope.f = {};
-				    };
 		});
+		// return our array which should be devoid of
+		// any duplicates
+		return output;
+	};
+});
+
+app
+		.controller(
+				'myCtrl',
+				function($scope) {
+					var companyList = document.getElementById("companyList");
+					ratelist = rateListMethod();
+					if (decodeURIComponent(window.location.search.substring(1))
+							.split("&").length > 1) {
+						url_safe_distributor = decodeURIComponent(
+								window.location.search.substring(1)).split("&")[0]
+								.split("=")[1];
+						url_safe_tarifftype = decodeURIComponent(
+								window.location.search.substring(1)).split("&")[1]
+								.split("=")[1];
+						url_safe_usage = decodeURIComponent(
+								window.location.search.substring(1)).split("&")[2]
+								.split("=")[1];
+						url_safe_days = decodeURIComponent(
+								window.location.search.substring(1)).split("&")[3]
+								.split("=")[1];
+					}
+					var plans = 0;
+					var spend = 0;
+					var res = jQuery
+							.grep(
+									ratelist.Rates,
+									function(v) {
+										if (v.Distributor == url_safe_distributor
+												&& v.TariffType == url_safe_tarifftype) {
+											plans++;
+											spend = ((v.Peak1 * url_safe_usage) + (v.Supply * url_safe_days));
+											v.spend = parseFloat(spend);
+											return v;
+										}
+									});
+					res.sort(function(a, b) {
+						return a.spend - b.spend;
+					});
+					res["Rates"] = res;
+					$scope.jsonbody = res;
+					$scope.plans = plans;
+					if (plans > 0) {
+						$scope.rate = "Rates";
+						$scope.exitfee = "Exit Fee";
+						$scope.discount = "Discount";
+
+					}
+
+					$scope.clearFilter = function() {
+
+						$scope.f = {};
+					};
+				
+			
+				});
